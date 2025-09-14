514ede610701f1074292c0fa1a3e43d22f865ff1
daniyalareeb <daniyalareeb123@gmail.com>
2025-09-14 22:39:09 +0100

    Add latest Vercel domain to CORS origins

--- backend/app/config.py
+++ backend/app/config.py
@@ -44,7 +44,8 @@ class Settings(BaseSettings):
         "https://portfolio-frontend-9s81f6rvc-daniyalareebs-projects.vercel.app",
         "https://portfolio-frontend-93ozpdybl-daniyalareebs-projects.vercel.app",
         "https://portfolio-frontend-fqjgi8t2r-daniyalareebs-projects.vercel.app",
-        "https://portfolio-frontend-gw0hmyqa6-daniyalareebs-projects.vercel.app"
+        "https://portfolio-frontend-gw0hmyqa6-daniyalareebs-projects.vercel.app",
+        "https://portfolio-frontend-6zjtin9bp-daniyalareebs-projects.vercel.app"
     ]
     
     # API
